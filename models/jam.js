d155cef0d428da1c96bf0d5c02e7a5d955175e9d
Samuel Gutierrez <69909329+S535530@users.noreply.github.com>
2022-11-30 14:47:02 -0600

    added a min max for Price and Size

--- models/jam.js
+++ models/jam.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 const jamSchema = mongoose.Schema({
-    Price: Number,
-    Size: Number, 
-    Flavor: String
-})
+    Price: {
+        type: Number, min: [1], max: 14,
+    },
+    Size: {
+        type: Number, min: [8], max: 140,
+    }, 
+    Flavor: {
+        type: String
+    },
+});
 module.exports = mongoose.model("Jam", jamSchema)
